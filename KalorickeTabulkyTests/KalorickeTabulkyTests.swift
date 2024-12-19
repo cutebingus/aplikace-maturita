0eb3ae7896de9e96c899a0266fcc0cda2176a617
cutebingus <cuter.bingus@gmail.com>
2024-12-19 06:34:12 +0100

    Initial commit

---
+++ KalorickeTabulkyTests/KalorickeTabulkyTests.swift
@@ -0,0 +1,17 @@
+//
+//  KalorickeTabulkyTests.swift
+//  KalorickeTabulkyTests
+//
+//  Created by Nicolas Jalůvka on 10.11.2024.
+//
+
+import Testing
+@testable import KalorickeTabulky
+
+struct KalorickeTabulkyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
