eb2f00b1b4bd8f18f2540413ac3d558ad62613fe
cutebingus <cuter.bingus@gmail.com>
2024-12-21 09:36:26 +0100

    kaloricke_tabulky0.2

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/ui/pages/profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +13,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
 
-
-
-
-
+        primarySwatch: Colors.blue,
         // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a purple toolbar. Then, without quitting the app,
         // try changing the seedColor in the colorScheme below to Colors.green
@@ -27,14 +26,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-
-
-
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Calorie counter'),
+      home: ProfileScreen()
     );
   }
 }
--- flutter_application_1/lib/ui/pages/profile_screen.dart
+++ flutter_application_1/lib/ui/pages/profile_screen.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
- 
-class ProfileScreen extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: const Color(0xFFE9E9E9),
-      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 40,
+        selectedIconTheme: IconThemeData(
+          color: const Color(0xFF200087)
+        ),
+
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "",
+          ),
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 0,
+            height: height * 0.35,
+            left: 0,
+            right: 0,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.vertical(
+                bottom: const Radius.circular(40),
+              ),
+              child: Container(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
